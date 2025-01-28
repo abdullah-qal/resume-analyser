45b0feeeeb2014256e58e6e3a3efb27d3d2019ea
Osama <osama.qalam@hotmail.com>
2025-01-27 22:10:09 -0500

    Create uploads folder if it doesn't exist

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
-
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const uploadsDir = path.join(__dirname, 'uploads');
+
+// Create the uploads directory if it doesn't exist
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir);
+}
 
 const fileStorageEngine = multer.diskStorage({
     destination: (req, file, cb) => {
