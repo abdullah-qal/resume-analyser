9b867dccb8528a39577660e7400807f77c317b9f
Osama <osama.qalam@hotmail.com>
2025-01-28 21:36:18 -0500

    Removed all expressJS leftovers

--- src/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const upload = require('../config/multerConfig');
-const { handleFileUpload } = require('../controllers/uploadController');
-
-const router = express.Router();
-
-router.post('/upload', upload.single('file'), handleFileUpload);
-
-module.exports = router;
